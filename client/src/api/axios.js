be1a49a4014c67335636db55f7ea257d693ef9a1
Shuvam <kundushibam7@gmail.com>
2026-08-20 16:59:58 +0530

    fixed

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 const api = axios.create({
     baseURL: (import.meta.env.VITE_BASE_URL || "http://localhost:4000") + "/api",
-    withCredentials: true,
 })
 
 // Attach Auth token to all network requests
